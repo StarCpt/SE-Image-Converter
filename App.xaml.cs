047b6154f4e595cbd5d843bb16022418d06968e0
StarCpt <86216339+StarCpt@users.noreply.github.com>
2024-04-07 22:57:49 -0700

    Replace logger with an improved one

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageConverterPlus.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
---
+++ Services/LogService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ImageConverterPlus.Services
+{
+    public sealed class LogService
+    {
+        public string LogPath { get; }
+
+        private readonly Channel<string> _logBuffer;
+        private readonly string _dateTimeFormat;
+        private readonly TimeZoneInfo _timeZone;
+        private readonly Task _bufferConsumeTask;
+
+        public LogService(string path, string dateTimeFormat, TimeZoneInfo timeZone, bool overwriteExistingFile)
+        {
+            LogPath = path;
+            _logBuffer = Channel.CreateUnbounded<string>(new UnboundedChannelOptions
+            {
+                SingleWriter = false,
+                SingleReader = true,
+            });
+            _dateTimeFormat = dateTimeFormat;
+            _timeZone = timeZone;
+
+            if (Path.GetDirectoryName(LogPath) is not string dir || string.IsNullOrWhiteSpace(dir))
+            {
+                throw new ArgumentException(null, nameof(path));
+            }
+            Directory.CreateDirectory(dir);
+            _bufferConsumeTask = ConsumeBuffer(overwriteExistingFile ? File.CreateText(LogPath) : File.AppendText(LogPath));
+
+            Log("Log Started");
+            Log(timeZone.ToString());
+        }
+
+        public T Log<T>(T e, bool dialog = false) where T : Exception
+        {
+            Log(e.ToString(), dialog);
+            return e;
+        }
+
+        public async void Log(string str, bool dialog = false)
+        {
+            string logStr = FormLogString(str);
+            await _logBuffer.Writer.WriteAsync(logStr);
+        }
+
+        private string FormLogString(string str)
+        {
+            return $"{TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, _timeZone).ToString(_dateTimeFormat)}   Thread {Environment.CurrentManagedThreadId,3}  {str}";
+        }
+
+        async Task ConsumeBuffer(StreamWriter writer)
+        {
+            ChannelReader<string> reader = _logBuffer.Reader;
+            await foreach (string entry in reader.ReadAllAsync().ConfigureAwait(false))
+            {
+                try
+                {
+                    Console.WriteLine(entry);
+                    await writer.WriteLineAsync(entry).ConfigureAwait(false);
+                }
+                catch (Exception e)
+                {
+                    Log(e);
+                    await Task.Delay(1000).ConfigureAwait(false);
+                }
+            }
+            writer.Flush();
+            writer.Dispose();
+        }
+
+        public void OpenLogFile()
+        {
+            if (!File.Exists(LogPath))
+                File.CreateText(LogPath);
+
+            Process.Start("notepad.exe", LogPath);
+        }
+
+        public void Close()
+        {
+            Log("Log Closed");
+            _logBuffer.Writer.Complete();
+            _bufferConsumeTask.Wait();
+        }
+    }
+}
